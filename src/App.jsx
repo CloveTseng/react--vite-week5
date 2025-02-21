cecb0508962c91320fb2b3c094ce76af92884cff
DolceTseng <jessica.turn613@gmail.com>
2025-02-21 15:17:48 +0800

    update weekwork

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,50 @@
 import { useEffect, useRef, useState } from 'react';
 import axios from 'axios';
 import { Modal } from 'bootstrap';
+import { useForm } from 'react-hook-form';
 
 const BASE_URL = import.meta.env.VITE_BASE_URL;
 const API_PATH = import.meta.env.VITE_API_PATH;
 
+const Input = ({ register, errors, inputText, id, type, rules }) => {
+  return (
+    <>
+      <div className="mb-3">
+        <label htmlFor={id} className="form-label">
+          {inputText}
+        </label>
+        <input
+          id={id}
+          type={type}
+          className={`form-control ${errors[id] && 'is-invalid'}`}
+          placeholder={`請輸入${inputText}`}
+          {...register(id, rules)}
+        />
+
+        {errors[id] && (
+          <p className="text-danger my-2">{errors?.[id]?.message}</p>
+        )}
+      </div>
+    </>
+  );
+};
+
 function App() {
   const [products, setProducts] = useState([]);
   const [tempProduct, setTempProduct] = useState([]);
+
+  const [cart, setCart] = useState({});
+  const [isCartEmpty, setIsCartEmpty] = useState(true);
+
+  const getCart = async () => {
+    try {
+      const res = await axios.get(`${BASE_URL}/v2/api/${API_PATH}/cart`);
+      setCart(res.data.data);
+      setIsCartEmpty(res.data.data.carts?.length === 0);
+    } catch (error) {
+      alert('取得購物車資訊失敗');
+    }
+  };
 
   useEffect(() => {
     const getProducts = async () => {
@@ -16,9 +53,11 @@ function App() {
         setProducts(res.data.products);
       } catch (error) {
         alert('取得產品失敗');
+      } finally {
       }
     };
     getProducts();
+    getCart();
   }, []);
 
   const productModalRef = useRef(null);
@@ -42,6 +81,101 @@ function App() {
   };
 
   const [qtySelect, setQtySelect] = useState(1);
+
+  const addCardItem = async (product_id, qty) => {
+    try {
+      await axios.post(`${BASE_URL}/v2/api/${API_PATH}/cart`, {
+        data: {
+          product_id,
+          qty: Number(qty),
+        },
+      });
+      getCart();
+    } catch (error) {
+      alert('加入購物車失敗');
+    }
+  };
+
+  const removeCard = async () => {
+    try {
+      await axios.delete(`${BASE_URL}/v2/api/${API_PATH}/carts`);
+      getCart();
+    } catch (error) {
+      alert('清空購物車失敗');
+    }
+  };
+
+  const removeCardItem = async (cartItem_id) => {
+    try {
+      await axios.delete(`${BASE_URL}/v2/api/${API_PATH}/cart/${cartItem_id}`);
+      getCart();
+    } catch (error) {
+      alert('刪除品項失敗');
+    }
+  };
+
+  const updateCardItem = async (cartItem_id, product_id, qty) => {
+    try {
+      await axios.put(`${BASE_URL}/v2/api/${API_PATH}/cart/${cartItem_id}`, {
+        data: {
+          product_id,
+          qty: Number(qty),
+        },
+      });
+      getCart();
+    } catch (error) {
+      alert('更新品項失敗');
+    }
+  };
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm({
+    defaultValues: {
+      email: '',
+      name: '',
+      tel: '',
+      address: '',
+      message: '',
+    },
+  });
+
+  const resetForm = () => {
+    reset({
+      email: '',
+      name: '',
+      tel: '',
+      address: '',
+      message: '',
+    });
+  };
+
+  const onSubmit = handleSubmit((data) => {
+    console.log(data);
+    const { message, ...user } = data;
+    const userInfo = {
+      data: {
+        user,
+        message,
+      },
+    };
+    checkout(userInfo);
+  });
+
+  const checkout = async (data) => {
+    try {
+      axios.post(`${BASE_URL}/v2/api/${API_PATH}/order`, data);
+      await removeCard();
+      resetForm();
+      getCart();
+      alert('已成功送出訂單');
+    } catch (error) {
+      alert('結帳有誤，請洽服務人員');
+    }
+  };
 
   return (
     <div className="container">
@@ -79,7 +213,11 @@ function App() {
                     >
                       查看更多
                     </button>
-                    <button type="button" className="btn btn-outline-danger">
+                    <button
+                      onClick={() => addCardItem(product.id, 1)}
+                      type="button"
+                      className="btn btn-outline-danger"
+                    >
                       加到購物車
                     </button>
                   </div>
@@ -139,133 +277,175 @@ function App() {
                 </div>
               </div>
               <div className="modal-footer">
-                <button type="button" className="btn btn-primary">
+                <button
+                  onClick={() => addCardItem(tempProduct.id, qtySelect)}
+                  type="button"
+                  className="btn btn-primary"
+                >
                   加入購物車
                 </button>
               </div>
             </div>
           </div>
         </div>
+        {cart.carts?.length > 0 && (
+          <div>
+            <div className="text-end py-3">
+              <button
+                onClick={removeCard}
+                className="btn btn-outline-danger"
+                type="button"
+              >
+                清空購物車
+              </button>
+            </div>
 
-        <div className="text-end py-3">
-          <button className="btn btn-outline-danger" type="button">
-            清空購物車
-          </button>
-        </div>
+            <table className="table align-middle">
+              <thead>
+                <tr>
+                  <th></th>
+                  <th>品名</th>
+                  <th style={{ width: '150px' }}>數量/單位</th>
+                  <th className="text-end">單價</th>
+                </tr>
+              </thead>
 
-        <table className="table align-middle">
-          <thead>
-            <tr>
-              <th></th>
-              <th>品名</th>
-              <th style={{ width: '150px' }}>數量/單位</th>
-              <th className="text-end">單價</th>
-            </tr>
-          </thead>
-
-          <tbody>
-            <tr>
-              <td>
-                <button type="button" className="btn btn-outline-danger btn-sm">
-                  x
-                </button>
-              </td>
-              <td></td>
-              <td style={{ width: '150px' }}>
-                <div className="d-flex align-items-center">
-                  <div className="btn-group me-2" role="group">
-                    <button
-                      type="button"
-                      className="btn btn-outline-dark btn-sm"
-                    >
-                      -
-                    </button>
-                    <span
-                      className="btn border border-dark"
-                      style={{ width: '50px', cursor: 'auto' }}
-                    ></span>
-                    <button
-                      type="button"
-                      className="btn btn-outline-dark btn-sm"
-                    >
-                      +
-                    </button>
-                  </div>
-                  <span className="input-group-text bg-transparent border-0">
-                    unit
-                  </span>
-                </div>
-              </td>
-              <td className="text-end">單項總價</td>
-            </tr>
-          </tbody>
-          <tfoot>
-            <tr>
-              <td colSpan="3" className="text-end">
-                總計：
-              </td>
-              <td className="text-end" style={{ width: '130px' }}></td>
-            </tr>
-          </tfoot>
-        </table>
+              <tbody>
+                {cart.carts?.map((cartItem) => (
+                  <tr key={cartItem.id}>
+                    <td>
+                      <button
+                        onClick={() => removeCardItem(cartItem.id)}
+                        type="button"
+                        className="btn btn-outline-danger btn-sm"
+                      >
+                        x
+                      </button>
+                    </td>
+                    <td>{cartItem.product.title}</td>
+                    <td style={{ width: '150px' }}>
+                      <div className="d-flex align-items-center">
+                        <div className="btn-group me-2" role="group">
+                          <button
+                            onClick={() =>
+                              updateCardItem(
+                                cartItem.id,
+                                cartItem.product_id,
+                                cartItem.qty - 1
+                              )
+                            }
+                            disabled={cartItem.qty === 1}
+                            type="button"
+                            className="btn btn-outline-dark btn-sm"
+                          >
+                            -
+                          </button>
+                          <span
+                            className="btn border border-dark"
+                            style={{ width: '50px', cursor: 'auto' }}
+                          >
+                            {cartItem.qty}
+                          </span>
+                          <button
+                            onClick={() =>
+                              updateCardItem(
+                                cartItem.id,
+                                cartItem.product_id,
+                                cartItem.qty + 1
+                              )
+                            }
+                            type="button"
+                            className="btn btn-outline-dark btn-sm"
+                          >
+                            +
+                          </button>
+                        </div>
+                        <span className="input-group-text bg-transparent border-0">
+                          {cartItem.product.unit}
+                        </span>
+                      </div>
+                    </td>
+                    <td className="text-end">{cartItem.total}</td>
+                  </tr>
+                ))}
+              </tbody>
+              <tfoot>
+                <tr>
+                  <td colSpan="3" className="text-end">
+                    總計：
+                  </td>
+                  <td className="text-end" style={{ width: '130px' }}>
+                    {cart.total}
+                  </td>
+                </tr>
+              </tfoot>
+            </table>
+          </div>
+        )}
       </div>
 
       <div className="my-5 row justify-content-center">
-        <form className="col-md-6">
-          <div className="mb-3">
-            <label htmlFor="email" className="form-label">
-              Email
-            </label>
-            <input
-              id="email"
-              type="email"
-              className="form-control"
-              placeholder="請輸入 Email"
-            />
-
-            <p className="text-danger my-2"></p>
-          </div>
-
-          <div className="mb-3">
-            <label htmlFor="name" className="form-label">
-              收件人姓名
-            </label>
-            <input
-              id="name"
-              className="form-control"
-              placeholder="請輸入姓名"
-            />
-
-            <p className="text-danger my-2"></p>
-          </div>
-
-          <div className="mb-3">
-            <label htmlFor="tel" className="form-label">
-              收件人電話
-            </label>
-            <input
-              id="tel"
-              type="text"
-              className="form-control"
-              placeholder="請輸入電話"
-            />
-
-            <p className="text-danger my-2"></p>
-          </div>
-
-          <div className="mb-3">
-            <label htmlFor="address" className="form-label">
-              收件人地址
-            </label>
-            <input
-              id="address"
-              type="text"
-              className="form-control"
-              placeholder="請輸入地址"
-            />
-
-            <p className="text-danger my-2"></p>
-          </div>
+        <form className="col-md-6" onSubmit={onSubmit}>
+          <Input
+            register={register}
+            errors={errors}
+            inputText="Email"
+            id="email"
+            type="email"
+            rules={{
+              required: {
+                value: true,
+                message: 'email 欄位必填',
+              },
+              pattern: {
+                value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+                message: 'Email 格式錯誤',
+              },
+            }}
+          />
+          <Input
+            register={register}
+            errors={errors}
+            inputText="收件人姓名"
+            id="name"
+            type="text"
+            rules={{
+              required: {
+                value: true,
+                message: '姓名欄位必填',
+              },
+            }}
+          />
+          <Input
+            register={register}
+            errors={errors}
+            inputText="收件人電話"
+            id="tel"
+            type="text"
+            rules={{
+              required: {
+                value: true,
+                message: '電話欄位必填',
+              },
+              pattern: {
+                value: /^(0[2-8]\d{7}|09\d{8})$/,
+                message: '請輸入正確電話',
+              },
+            }}
+          />
+          <Input
+            register={register}
+            errors={errors}
+            inputText="收件人地址"
+            id="address"
+            type="text"
+            rules={{
+              required: {
+                value: true,
+                message: '地址欄位必填',
+              },
+            }}
+          />
 
           <div className="mb-3">
             <label htmlFor="message" className="form-label">
@@ -276,10 +456,15 @@ function App() {
               className="form-control"
               cols="30"
               rows="10"
+              {...register('message')}
             ></textarea>
           </div>
           <div className="text-end">
-            <button type="submit" className="btn btn-danger">
+            <button
+              type="submit"
+              className="btn btn-danger"
+              disabled={isCartEmpty}
+            >
               送出訂單
             </button>
           </div>
